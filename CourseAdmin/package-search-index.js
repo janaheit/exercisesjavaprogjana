30ca48e16379ef34573bcb54d7346d9aa563e931
Jana Heitkemper <janaheitkemper@gmail.com>
2022-09-13 14:51:14 +0200

    deleted javadoc files

--- CourseAdmin/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"be.abis.courseadmin.model"},{"l":"be.abis.courseadmin.test"}];updateSearchResults();
--- CourseAdmin/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"be.abis.courseadmin.model","l":"Company"},{"p":"be.abis.courseadmin.model","l":"Course"},{"p":"be.abis.courseadmin.model","l":"Person"},{"p":"be.abis.courseadmin.test","l":"TestCourse"}];updateSearchResults();
