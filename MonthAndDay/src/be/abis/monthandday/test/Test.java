e3f0af55c1c1abdbef3f07883773401c4ba1c5a8
Jana Heitkemper <janaheitkemper@gmail.com>
2022-09-26 16:45:56 +0200

    added month and day implementation

--- MonthAndDay/src/be/abis/monthandday/test/Test.java
+++ MonthAndDay/src/be/abis/monthandday/test/Test.java
@@ -3,15 +3,27 @@ package be.abis.monthandday.test;
 import be.abis.monthandday.repo.MonthDayRepo;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
         HashMap<String, Integer> calendar = MonthDayRepo.getInstance().getCalendar();
 
         // make scanner
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Type day and month (letters) to find out how many days are still left in the month.");
+
+        int day = scanner.nextInt();
+        String month = scanner.next();
+
+        System.out.println("You have " + calculateDaysTillEndOfMonth(calendar, day, month) + " days left in "
+        + month + ".");
+
+
     }
 
-    public int calculateDaysTillEndOfMonth(HashMap<String, Integer> calendar, String month, int day){
+    public static int calculateDaysTillEndOfMonth(HashMap<String, Integer> calendar, int day, String month){
         int numOfDaysInMonth = calendar.get(month);
         return numOfDaysInMonth-day;
     }
